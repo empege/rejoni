5785389260a5cfc2befbe10f4d9e285233c3624e
Ivan <ivan.mitov@hotmail.com>
2023-05-18 00:41:24 +0200

    added label to each "rejon"

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // Baza podataka ulica i rejona kojima pripadaju
-const rejon_1 = [
-  'Zorana Djindjica 1 - 115',
-  'Ime ulice',
-]
+const rejon_1 = {
+  label: '1',
+  streets: [
+    'Zorana Djindjica 1 - 115',
+    'Ime ulice',
+  ]
+}
 
-const rejon_2 = [
-  'Zorana Djindjica 116 - 500',
-  'Ime ulice neke druge',
-]
+const ledine = {
+  label: 'Ledine',
+  streets: [
+    'Zorana Djindjica 116 - 500',
+    'Ime ulice neke druge',
+  ]
+}
 
 const allDistricts = [
   rejon_1,
-  rejon_2,
+  ledine,
   // rejon_3,
   // rejon_4,
   // rejon_5,
@@ -39,9 +45,19 @@ const createElement = (tag = 'div', classes, text, data) => {
   return el
 }
 
-const sortByDistrict = (a,b) => {
-  a.district - b.district
+function sortByDistrict(a, b) {
+  if (a.label < b.label) {
+    return -1;
+  }
+  if (a.label > b.label) {
+    return 1;
+  }
+  return 0;
 }
+
+// const sortByDistrict = (a,b) => {
+//   a.district - b.district
+// }
 
 const handleChange = (e) => {
   const searchValue = e.target.value.toUpperCase()
@@ -57,26 +73,28 @@ const handleChange = (e) => {
 
 const createItems = (items) => {
   items.forEach(item => {
-    const streetToUppercase = item.street.toUpperCase()
-    const wrapper = createElement('div', ['districts__items-wrapper'], undefined, streetToUppercase)
-    const street = createElement('span', ['districts__item', 'districts__item--street'], item.street)
-    const district = createElement('span', ['districts__item', 'districts__item--district-number'], item.district)
-    wrapper.appendChild(street)
-    wrapper.appendChild(district)
-    resultsContent.append(wrapper)
+    item.streets.forEach(currentStreet => {
+      const streetToUppercase = currentStreet.toUpperCase()
+      const wrapper = createElement('div', ['districts__items-wrapper'], undefined, streetToUppercase)
+      const street = createElement('span', ['districts__item', 'districts__item--street'], currentStreet)
+      const district = createElement('span', ['districts__item', 'districts__item--district-number'], item.label)
+      wrapper.appendChild(street)
+      wrapper.appendChild(district)
+      resultsContent.append(wrapper)
+    })
   })
 }
 
 // Event listeners
 searchInput.addEventListener('input', handleChange)
-
+console.log(allDistricts)
 // Prodji kroz sve rejone, dodaj svakoj ulici njen rejon radi lakse pretrage posle
-const districts = [].concat(...allDistricts.map((district, id) => {
-  return district.map(street => {
-    return { street, district: id + 1}
-  })
-}))
+// const districts = [].concat(...allDistricts.map(district => {
+//   return district.streets.map(district => {
+//     return { street: district.street, district: district.label}
+//   })
+// }))
 // Sortiraj prema rejonu i ispisi sve
-districts.sort(sortByDistrict)
-createItems(districts)
+allDistricts.sort(sortByDistrict)
+createItems(allDistricts)
 const districtElements = document.querySelectorAll('.results__content .districts__items-wrapper')
